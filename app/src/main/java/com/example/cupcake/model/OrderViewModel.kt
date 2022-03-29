e961891c781859e26e91bfe3e952700a532b8e78
EDUARDO CHAVEZ <41527710+RainbowChvz@users.noreply.github.com>
2022-03-29 15:11:54 -0700

    Update price views with data binding, by setting lifeCycleOwner

--- app/src/main/java/com/example/cupcake/model/OrderViewModel.kt
+++ app/src/main/java/com/example/cupcake/model/OrderViewModel.kt
@@ -6,6 +6,9 @@ import androidx.lifecycle.ViewModel
 import java.text.SimpleDateFormat
 import java.util.*
 
+private const val PRICE_PER_CUPCAKE = 2.00
+private const val PRICE_FOR_SAME_DAY_PICKUP = 3.00
+
 class OrderViewModel: ViewModel() {
 
     private var _quantity = MutableLiveData<Int>()
@@ -25,6 +28,7 @@ class OrderViewModel: ViewModel() {
 
     fun setQuantity(numberCupcakes: Int) {
         _quantity.value = numberCupcakes
+        updatePrice()
     }
 
     fun setFlavor(desiredFlavor: String) {
@@ -33,6 +37,7 @@ class OrderViewModel: ViewModel() {
 
     fun setDate(pickUpDate: String) {
         _date.value = pickUpDate
+        updatePrice()
     }
 
     fun hasNoFlavorSet(): Boolean {
@@ -57,5 +62,15 @@ class OrderViewModel: ViewModel() {
         _flavor.value = ""
         _date.value = dateOptions[0]
         _price.value = 0.0
+    }
+
+    private fun updatePrice() {
+        var calculatedPrice = (quantity.value?:0) * PRICE_PER_CUPCAKE
+
+        if (dateOptions[0] == date.value)
+            calculatedPrice += PRICE_FOR_SAME_DAY_PICKUP
+
+        _price.value = calculatedPrice
+
     }
 }
