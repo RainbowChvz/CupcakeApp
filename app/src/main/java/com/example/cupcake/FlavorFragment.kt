4c4befdbb7974c0ba17615865ceaf07b0e8c684b
EDUARDO CHAVEZ <41527710+RainbowChvz@users.noreply.github.com>
2022-03-30 10:33:10 -0700

    Add listener binding to Cancel button

--- app/src/main/java/com/example/cupcake/FlavorFragment.kt
+++ app/src/main/java/com/example/cupcake/FlavorFragment.kt
@@ -65,6 +65,14 @@ class FlavorFragment : Fragment() {
     }
 
     /**
+     * Cancel order and go back to StartFragment
+     */
+    fun cancelOrder() {
+        sharedViewModel.resetOrder()
+        findNavController().navigate(R.id.action_flavorFragment_to_startFragment)
+    }
+
+    /**
      * This fragment lifecycle method is called when the view hierarchy associated with the fragment
      * is being removed. As a result, clear out the binding object.
      */
